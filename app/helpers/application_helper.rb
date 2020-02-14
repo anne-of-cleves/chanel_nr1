cfd1d890d5ef87873e1ff8d0293d56d4f8d6b21b
Ana Conti <ac.pub@web.de>
2020-02-14 19:18:34 +0100

    bootstrap awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def icon_tag(icon_name, context_class = 'primary', title = '')
+    class_str = "fas fa-#{icon_name} text-#{context_class} fa-lg"
+    content_tag(:i, '', class: class_str, title: title)
+  end
 end
